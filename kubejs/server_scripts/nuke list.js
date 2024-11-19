8dbce47019694c4f26a43543f34ccbdfce48d728
George Leask <squawkykaka@gmail.com>
2024-11-20 10:54:23 +1300

    Added enchantment industry and found the sophisticated backpacks config

--- kubejs/server_scripts/nuke list.js
+++ kubejs/server_scripts/nuke list.js
@@ -5,5 +5,26 @@
 
    // It relies on the other "nuke" files in various KubeJS script folders to function properly.
    global.itemsToRemove = [   
-      'toms_storage:ts.wireless_terminal'
+      'toms_storage:ts.wireless_terminal',
+      ['sophisticatedbackpacks:advanced_filter_upgrade', 'sophisticatedbackpacks:battery_upgrade', 
+         'sophisticatedbackpacks:anvil_upgrade', 'sophisticatedbackpacks:xp_pump_upgrade', 
+         'sophisticatedbackpacks:advanced_pump_upgrade', 'sophisticatedbackpacks:pump_upgrade', 
+         'sophisticatedbackpacks:tank_upgrade', 'sophisticatedbackpacks:advanced_tool_swapper_upgrade', 
+         'sophisticatedbackpacks:stack_upgrade_tier_4', 'sophisticatedbackpacks:stack_upgrade_tier_3', 
+         'sophisticatedbackpacks:stack_upgrade_tier_2', 'sophisticatedbackpacks:stack_upgrade_tier_1', 
+         'sophisticatedbackpacks:stack_upgrade_starter_tier', 'sophisticatedbackpacks:stonecutter_upgrade', 
+         'sophisticatedbackpacks:crafting_upgrade', 'sophisticatedbackpacks:auto_blasting_upgrade', 
+         'sophisticatedbackpacks:blasting_upgrade', 'sophisticatedbackpacks:auto_smoking_upgrade', 
+         'sophisticatedbackpacks:smoking_upgrade', 'sophisticatedbackpacks:auto_smelting_upgrade', 
+         'sophisticatedbackpacks:smelting_upgrade', 'sophisticatedbackpacks:inception_upgrade', 
+         'sophisticatedbackpacks:refill_upgrade', 'sophisticatedbackpacks:advanced_deposit_upgrade', 
+         'sophisticatedbackpacks:deposit_upgrade', 'sophisticatedbackpacks:advanced_restock_upgrade', 
+         'sophisticatedbackpacks:restock_upgrade', 'sophisticatedbackpacks:advanced_void_upgrade', 
+         'sophisticatedbackpacks:void_upgrade', 'sophisticatedbackpacks:advanced_compacting_upgrade', 
+         'sophisticatedbackpacks:compacting_upgrade', 'sophisticatedbackpacks:feeding_upgrade', 
+         'sophisticatedbackpacks:advanced_feeding_upgrade', 'sophisticatedbackpacks:advanced_magnet_upgrade', 
+         'sophisticatedbackpacks:magnet_upgrade', 'sophisticatedbackpacks:advanced_pickup_upgrade', 
+         'sophisticatedbackpacks:advanced_pickup_upgrade', 'sophisticatedbackpacks:filter_upgrade',
+         'sophisticatedbackpacks:pickup_upgrade'],
+         'sophisticatedbackpacks:upgrade_base'
    ] 
