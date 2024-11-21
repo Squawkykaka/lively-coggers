e7973bce41881b4ded40a6f189d8baf31e3cba2d
George Leask <squawkykaka@gmail.com>
2024-11-21 13:18:21 +1300

    Added sturdy backpack recipe

--- kubejs/client_scripts/rename.js
+++ kubejs/client_scripts/rename.js
@@ -2,4 +2,7 @@ ClientEvents.lang("en_us", e => {
     e.renameItem('sophisticatedbackpacks:iron_backpack', 'Andesite Backpack')
     e.renameItem('sophisticatedbackpacks:gold_backpack', 'Brass Backpack')
     e.renameItem('sophisticatedbackpacks:diamond_backpack', 'Sturdy Backpack')
+    
+    e.renameItem("immersive_aircraft:nether_engine", "Combustion Engine")
+    e.renameItem("immersive_aircraft:eco_engine", "Steam Engine")
 })
--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -1,5 +1,8 @@
 ServerEvents.recipes(event => {
     event.remove({id: 'sophisticatedbackpacks:iron_backpack_from_copper'})
+    event.remove({input: 'create_enchantment_industry:experience_rotor'})
+    event.remove({id: 'sophisticatedbackpacks:gold_backpack'})
+    event.remove({id: 'sophisticatedbackpacks:diamond_backpack'})
 })
 
 ServerEvents.recipes(event => {
@@ -24,7 +27,7 @@ ServerEvents.recipes(event => {
         '#forge:plates/obsidian'
     ])
 
-    event.remove({input: "create_enchantment_industry:experience_rotor"})
+    // Copper backpack recipe
     event.shaped("sophisticatedbackpacks:copper_backpack", [
         '#S#',
         'LCL',
@@ -36,6 +39,7 @@ ServerEvents.recipes(event => {
         'S': 'minecraft:string'
     })
 
+    // Andesite backpack recipe
     event.custom({
         "type": "sophisticatedbackpacks:backpack_upgrade",
         "conditions": [
@@ -72,5 +76,81 @@ ServerEvents.recipes(event => {
         ], "result": {
           "item": "sophisticatedbackpacks:iron_backpack"
         }
-      });
+    });
+
+    // Brass backpack recipe
+    event.custom({
+      "type": "sophisticatedbackpacks:backpack_upgrade",
+      "conditions": [
+        {
+          "type": "sophisticatedcore:item_enabled",
+          "itemRegistryName": "sophisticatedbackpacks:iron_backpack"
+        }
+      ],
+      "key": {
+        "B": {
+          "item": "sophisticatedbackpacks:iron_backpack"
+        },
+        "A": {
+          "item": "create:steam_engine"
+        },
+        "S": {
+          "item": "create:redstone_link"
+        },
+        "C": {
+          "tag": "forge:ingots/brass"
+        },
+        "O": {
+          "item": "create:precision_mechanism"
+        },
+        "F": {
+          "item": "minecraft:leather"
+        }
+        
+      },
+      "pattern": [
+        "ASA",
+        "CBC",
+        "FOF"
+      ], "result": {
+        "item": "sophisticatedbackpacks:gold_backpack"
+      }
+    });
+
+    // Sturdy backpack
+    event.custom({
+      "type": "sophisticatedbackpacks:backpack_upgrade",
+      "conditions": [
+        {
+          "type": "sophisticatedcore:item_enabled",
+          "itemRegistryName": "sophisticatedbackpacks:diamond_backpack"
+        }
+      ],
+      "key": {
+        "B": {
+          "item": "sophisticatedbackpacks:gold_backpack"
+        },
+        "S": {
+          "item": "immersive_aircraft:nether_engine"
+        },
+        "C": {
+          "item": "create:railway_casing"
+        },
+        "O": {
+          "item": "toms_storage:ts.inventory_connector"
+        },
+        "F": {
+          "item": "create:sturdy_sheet"
+        }
+        
+      },
+      "pattern": [
+        " S ",
+        "CBC",
+        "FOF"
+      ], "result": {
+        "item": "sophisticatedbackpacks:diamond_backpack"
+      }
+    });
+
 })
