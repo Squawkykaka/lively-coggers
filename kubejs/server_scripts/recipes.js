fc68f55cb9f3a9125c2b5bde23a641f597616953
squawkykaka <squawkykaka@gmail.com>
2024-11-23 18:49:55 +1300

    Added computercraft

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -3,6 +3,7 @@ ServerEvents.recipes(event => {
     event.remove({input: 'create_enchantment_industry:experience_rotor'})
     event.remove({id: 'sophisticatedbackpacks:gold_backpack'})
     event.remove({id: 'sophisticatedbackpacks:diamond_backpack'})
+    event.remove({id: 'toms_storage:inventory_cable '})
 })
 
 ServerEvents.recipes(event => {
@@ -153,4 +154,17 @@ ServerEvents.recipes(event => {
       }
     });
 
+})
+
+ServerEvents.recipes(event => {
+  const transition = 'kubejs:incomplete_item_cable'
+  event.recipes.create.sequenced_assembly([
+    Item.of("toms_storage:ts.inventory_cable", 8).withChance(0.95),
+    Item.of("create:brass_ingot", 1).withChance(0.05)
+  ], "create:fluid_pipe", [
+    event.recipes.create.cutting(transition, transition),
+    event.recipes.create.deploying(transition, [transition, Item.of("create:sturdy_sheet")]),
+    event.recipes.create.filling(transition, [transition, Fluid.of("create_enchantment_industry:flowing_experience", 50)]),
+    event.recipes.create.pressing(transition, transition)
+  ]).transitionalItem(transition).loops(1)
 })
