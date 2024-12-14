c6520b474caa4c47c1e07ef1638627a29713f490
George <squawkykaka@gmail.com>
2024-12-14 20:50:08 +1300

    Added toms storage blocks to the wrench_pickup tag

---
+++ kubejs/server_scripts/tags_modify.js
@@ -0,0 +1,7 @@
+ServerEvents.tags('block', event => {
+    const tomblocks = ['toms_storage:ts.inventory_cable_connector_framed', 'toms_storage:ts.inventory_proxy', 'toms_storage:ts.crafting_terminal', 'toms_storage:ts.inventory_hopper_basic', 'toms_storage:ts.level_emitter', 'toms_storage:ts.inventory_connector', 'toms_storage:ts.trim', 'toms_storage:ts.open_crate', 'toms_storage:ts.inventory_cable', 'toms_storage:ts.inventory_cable_framed', 'toms_storage:ts.inventory_cable_connector', 'toms_storage:ts.inventory_cable_connector_filtered']
+
+    tomblocks.forEach(block => {
+        event.add("create:wrench_pickup", block)
+    })
+})
