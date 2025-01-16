99864bcab65b639a13872591e3fe71dcf7618b2a
George <squawkykaka@gmail.com>
2025-01-17 11:07:02 +1300

    changed required tool for toms storage to be pickaxe

--- kubejs/server_scripts/tagschanges.js
+++ kubejs/server_scripts/tagschanges.js
@@ -4,4 +4,21 @@ ServerEvents.tags('block', event => {
     tomblocks.forEach(block => {
         event.add("create:wrench_pickup", block)
     })
+
+    const pickonly = ["toms_storage:ts.crafting_terminal",
+        "toms_storage:ts.inventory_cable_connector_filtered",
+        "toms_storage:ts.inventory_cable_connector_framed",
+        "toms_storage:ts.inventory_cable_connector",
+        "toms_storage:ts.inventory_cable_framed",
+        "toms_storage:ts.inventory_cable",
+        "toms_storage:ts.inventory_connector",
+        "toms_storage:ts.inventory_hopper_basic",
+        "toms_storage:ts.inventory_proxy",
+        "toms_storage:ts.level_emitter",
+        "toms_storage:ts.storage_terminal"]
+
+    pickonly.forEach(block => {
+        event.remove("minecraft:mineable/axe", block)
+        event.add("minecraft:mineable/pickaxe", block)
+    })
 })
