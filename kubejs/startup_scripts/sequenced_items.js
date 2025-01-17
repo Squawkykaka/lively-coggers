b751a163c5e41acc3ce07deb062c00c3640b8738
George <squawkykaka@gmail.com>
2025-01-18 11:40:19 +1300

    Added textures for incomplte items

--- kubejs/startup_scripts/sequenced_items.js
+++ kubejs/startup_scripts/sequenced_items.js
@@ -1,4 +1,4 @@
 StartupEvents.registry("item", (event) => {
-  event.create("incomplete_item_cable", "create:sequenced_assembly").parentModel("create:fluid_pipe").texture("create:block/fluid_pipe");
-  event.create("incomplete_inventory_connector", "create:sequenced_assembly").parentModel("create:brass_block").texture("create:block/brass_block");
+  event.create("incomplete_item_cable", "create:sequenced_assembly").parentModel("create:item/fluid_pipe");
+  event.create("incomplete_inventory_connector", "create:sequenced_assembly").parentModel("create:block/brass_block")
 });
