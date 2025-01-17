5b6dbeff97923954a10b4eba293ab6715fd6917e
Lint Action <lint-action@samuelmeuli.com>
2025-01-17 05:19:35 +0000

    Fix code style issues with Prettier

--- kubejs/client_scripts/JEI - nuke.js
+++ kubejs/client_scripts/JEI - nuke.js
@@ -1,8 +1,5 @@
-JEIEvents.hideItems(event => {
-
+JEIEvents.hideItems((event) => {
   // Works with the "nuke list" file to remove all items you want to remove from being visible in JEI (also works with EMI, if JEI is installed as well)
 
-  event.hide([
-    global.itemsToRemove,
-  ])
-})
+  event.hide([global.itemsToRemove]);
+});
