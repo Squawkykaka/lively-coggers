02c49d20d6ad975f3cf2569b2288c6ef2dba2584
Lint Action <lint-action@samuelmeuli.com>
2025-01-21 02:53:29 +0000

    Fix code style issues with Prettier

--- kubejs/server_scripts/recipes/ae2.js
+++ kubejs/server_scripts/recipes/ae2.js
@@ -1,39 +1,88 @@
 ServerEvents.recipes((event) => {
-    const transition = "kubejs:incomplete_silicon_press";
+  const transition = "kubejs:incomplete_silicon_press";
 
-    event.recipes.create.sequenced_assembly([Item.of("ae2:silicon_press", 1)], "create:sturdy_sheet", [
+  event.recipes.create
+    .sequenced_assembly(
+      [Item.of("ae2:silicon_press", 1)],
+      "create:sturdy_sheet",
+      [
         event.recipes.create.deploying(transition, [transition, "ae2:silicon"]),
-        event.recipes.create.filling(transition, [Fluid.of("create_enchantment_industry:experience", 150), transition]),
-    ]).transitionalItem(transition).loops(1);
-})
+        event.recipes.create.filling(transition, [
+          Fluid.of("create_enchantment_industry:experience", 150),
+          transition,
+        ]),
+      ],
+    )
+    .transitionalItem(transition)
+    .loops(1);
+});
 
 ServerEvents.recipes((event) => {
-    const transition = "kubejs:incomplete_engineering_press";
+  const transition = "kubejs:incomplete_engineering_press";
 
-    event.recipes.create.sequenced_assembly([Item.of("ae2:engineering_processor_press", 1)], "create:sturdy_sheet", [
+  event.recipes.create
+    .sequenced_assembly(
+      [Item.of("ae2:engineering_processor_press", 1)],
+      "create:sturdy_sheet",
+      [
         event.recipes.create.deploying(transition, [transition, "diamond"]),
-        event.recipes.create.filling(transition, [Fluid.of("create_enchantment_industry:experience", 150), transition]),
-    ]).transitionalItem(transition).loops(1);
-})
+        event.recipes.create.filling(transition, [
+          Fluid.of("create_enchantment_industry:experience", 150),
+          transition,
+        ]),
+      ],
+    )
+    .transitionalItem(transition)
+    .loops(1);
+});
 
 ServerEvents.recipes((event) => {
-    const transition = "kubejs:incomplete_logic_press";
+  const transition = "kubejs:incomplete_logic_press";
 
-    event.recipes.create.sequenced_assembly([Item.of("ae2:logic_processor_press", 1)], "create:sturdy_sheet", [
-        event.recipes.create.deploying(transition, [transition, "create:golden_sheet"]),
-        event.recipes.create.filling(transition, [Fluid.of("create_enchantment_industry:experience", 150), transition]),
-    ]).transitionalItem(transition).loops(1);
-})
+  event.recipes.create
+    .sequenced_assembly(
+      [Item.of("ae2:logic_processor_press", 1)],
+      "create:sturdy_sheet",
+      [
+        event.recipes.create.deploying(transition, [
+          transition,
+          "create:golden_sheet",
+        ]),
+        event.recipes.create.filling(transition, [
+          Fluid.of("create_enchantment_industry:experience", 150),
+          transition,
+        ]),
+      ],
+    )
+    .transitionalItem(transition)
+    .loops(1);
+});
 
 ServerEvents.recipes((event) => {
-    const transition = "kubejs:incomplete_calculation_press";
+  const transition = "kubejs:incomplete_calculation_press";
 
-    event.recipes.create.sequenced_assembly([Item.of("ae2:calculation_processor_press", 1)], "create:sturdy_sheet", [
-        event.recipes.create.deploying(transition, [transition, "ae2:charged_certus_quartz_crystal"]),
-        event.recipes.create.filling(transition, [Fluid.of("create_enchantment_industry:experience", 150), transition]),
-    ]).transitionalItem(transition).loops(1);
-})
+  event.recipes.create
+    .sequenced_assembly(
+      [Item.of("ae2:calculation_processor_press", 1)],
+      "create:sturdy_sheet",
+      [
+        event.recipes.create.deploying(transition, [
+          transition,
+          "ae2:charged_certus_quartz_crystal",
+        ]),
+        event.recipes.create.filling(transition, [
+          Fluid.of("create_enchantment_industry:experience", 150),
+          transition,
+        ]),
+      ],
+    )
+    .transitionalItem(transition)
+    .loops(1);
+});
 
 ServerEvents.recipes((event) => {
-    event.recipes.create.item_application("ae2:flawed_budding_quartz", ["minecraft:budding_amethyst", "minecraft:sculk_shrieker"])
-})
+  event.recipes.create.item_application("ae2:flawed_budding_quartz", [
+    "minecraft:budding_amethyst",
+    "minecraft:sculk_shrieker",
+  ]);
+});
